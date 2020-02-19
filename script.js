86cd8653a338c1ffce030689ddcb84dfe091583c
Jan Wojcicki <unikori@gmail.com>
2020-02-19 19:59:01 +0100

    test

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ var ageJohn = 33;
 var ageMark = 55;
 var older = ageJohn > ageMark;
 console.log(typeof older);
-*/
+////////////////
+// BMI
+/////////////////
 var markMass = 70;
 var markHeight = 1.7;
 var johnMass = 80;
@@ -30,3 +32,104 @@ console.log(BMIJ);
 var markIsBigger = BMIM >= BMIJ;
 console.log(markIsBigger);
 console.log("Is Mark's BMI is higher than John's? " + markIsBigger);
+
+////////////
+//if statements
+////////////
+var firstName = 'John';
+var age = 14;
+
+age >= 18 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice');
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+var job = 'teacher';
+switch (job){
+    case 'teacher' :
+    console.log(firstName + ' teaches kids how to code');
+    break;
+    case 'driver':
+    console.log(firstName + ' drives');
+    break;
+    case 'designer':
+    console.log(firstName + ' designs beautiful websites');
+    break;
+    default:
+    console.log(firstName + ' does something else');
+}
+*/
+////////////////////
+// basketball
+//////////////////
+/*
+var john1 = 100;
+var john2 = 100;
+var john3 = 100;
+
+var mike1 = 100;
+var mike2 = 100;
+var mike3 = 100;
+
+var mary1 = 100;
+var mary2 = 100;
+var mary3 = 100;
+
+var jonas1 = 100;
+var jonas2 = 100;
+var jonas3 = 100;
+
+var johnAverage = (john1 + john2 + john3) / 3;
+console.log(johnAverage);
+
+var mikeAverage = (mike1 + mike2 + mike3) / 3;
+console.log(mikeAverage);
+
+var maryAverage = (mary1 + mary2 + mary3) / 3;
+console.log(maryAverage);
+
+var jonasAverage = (jonas1 + jonas2 + jonas3) / 3;
+console.log(jonasAverage);
+
+if(johnAverage > mikeAverage && johnAverage > maryAverage){
+    console.log('john wins the game with ' + johnAverage + ' points.');
+} else if(johnAverage === mikeAverage && maryAverage && jonasAverage){
+    console.log('DRAW!');
+} else if(mikeAverage > johnAverage &&  mikeAverage > maryAverage ){
+    console.log('mike wins the game with ' + mikeAverage + ' points.');
+} else if(maryAverage > johnAverage && maryAverage > mikeAverage ){
+    console.log('mary wins the game with ' + maryAverage + ' points.');
+} else {
+    console.log('lol');
+}
+*/
+function calculateAge(birthYear){
+    return 2020 - birthYear;
+}
+
+var ageJohn = calculateAge(1996);
+var ageMark = calculateAge(2001);
+var ageKate = calculateAge(1971);
+console.log(ageJohn, ageMark, ageKate);
+
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    console.log(ageJohn, ageMark, ageKate);   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
